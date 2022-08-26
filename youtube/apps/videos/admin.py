2d22d9049acaa82f626b4995360ac41fa6a82943
Nishant127 <handgenishant@gmail.com>
2022-08-27 01:06:03 +0530

    NH|Create database for video

--- youtube/apps/videos/admin.py
+++ youtube/apps/videos/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from videos.models import Video
 
-# Register your models here.
+admin.site.register(Video)
--- youtube/apps/videos/models.py
+++ youtube/apps/videos/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from youtube.common.models import TimeStampedModel
 
-# Create your models here.
+
+class Video(TimeStampedModel):
+
+    video_id = models.CharField(_("Video ID"), max_length=255, unique=True)
+    title = models.CharField(_("Title"), max_length=255)
+    description = models.TextField(_("Description"), blank=True, null=True)
+    thumbnail = models.JSONField(
+        _("Thumbnail"),
+        blank=True,
+        default=dict,
+        help_text=_("Thumbnails of different resolutions."),
+    )
+    published_at = models.DateTimeField(_("Published at"), blank=True, null=True)
+
+    class Meta:
+        verbose_name = _("Video")
+        verbose_name_plural = _("Videos")
+
+    def __str__(self):
+        return self.title
