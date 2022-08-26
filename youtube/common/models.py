3740fe89b28fcd7e381d3ff03e6591688d92ae2f
Nishant127 <handgenishant@gmail.com>
2022-08-27 01:06:03 +0530

    NH|Create database schema for video details

---
+++ youtube/common/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class TimeStampedModel(models.Model):
+
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
+
+    class Meta:
+        abstract = True
